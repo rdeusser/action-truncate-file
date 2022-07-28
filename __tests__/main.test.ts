ade29a5b12457b79079e6519586c4f7eb04d9fbb
Robert Deusser <robertjdeusser@gmail.com>
2022-07-28 11:38:55 -0400

    feat: write back original testdata

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,20 +1,30 @@
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
-import {expect, test} from '@jest/globals'
+import fs from 'fs'
+import { expect, test } from '@jest/globals'
 
 test('truncates file and removes last line', () => {
-  process.env['INPUT_FILE'] = path.join(__dirname, 'testdata', 'changelog.md')
-  process.env['INPUT_CHARACTERLIMIT'] = '1234'
-  process.env['INPUT_REMOVELASTLINE'] = 'false'
+    const file = path.join(__dirname, 'testdata', 'changelog.md')
+    const originalOutput = fs.readFileSync(file).toString()
 
-  const node = process.execPath
-  const main = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
+    process.env['INPUT_FILE'] = file
+    process.env['INPUT_CHARACTERLIMIT'] = '1234'
+    process.env['INPUT_REMOVELASTLINE'] = 'false'
 
-  const output = cp.execFileSync(node, [main], options).toString()
+    const node = process.execPath
+    const main = path.join(__dirname, '..', 'lib', 'main.js')
+    const options: cp.ExecFileSyncOptions = {
+        env: process.env
+    }
 
-  expect(output.length).toBe(1234)
+    const output = cp.execFileSync(node, [main], options).toString()
+    let charCount = 0
+    for (let char of output) {
+        charCount += char.length
+    }
+
+    fs.writeFileSync(file, originalOutput)
+
+    expect(charCount).toBe(1234)
 })
